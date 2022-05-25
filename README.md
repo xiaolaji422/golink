56a46b4ad4318540c571d1db4a9619836961a725
lajipeng <lajipeng@tencent.com>
2022-05-25 23:31:24 +0800

    protoc 更新

--- README.md
+++ README.md
@@ -4,3 +4,11 @@
  - 多系统接入，每个系统有属于自己的用户管理，权限校验。
  - 简易的路由管理，可以像http接口一样管理websocket接口。
  - 可以实时的向不同系统的不同用户推送消息。
+
+
+ ##  proto 安装
+ - https://github.com/protocolbuffers/protobuf/releases 下载解压
+ - go install github.com/golang/protobuf/protoc-gen-go    
+ - 检查gopath目录（%GOPATH%\）的bin文件夹，是否有protoc-gen-go.exe，protoc-gen-go-grpc.exe,拷贝至goroot（%GOROOT%\）的bin目录下
+ - cd pb && protoc --go_out=plugins=grpc:. send_msg.proto  
+
--- grpc/send_msg.go
+++ grpc/send_msg.go
@@ -20,3 +20,21 @@ func (*grpcSer) SendMsg(ctx context.Context, in *pb.SendMsgReq) (*pb.SendMsgRep,
 	err = wxRsp.Conn.Write([]byte(in.Message))
 	return res, err
 }
+
+// 发送给指定的很多人
+func (*grpcSer) SendAll(ctx context.Context, in *pb.SendMsgAllReq) (*pb.SendMsgAllRep, error) {
+	//
+	var res *pb.SendMsgAllRep = &pb.SendMsgAllRep{}
+	for _, v := range in.Userid {
+		wxRsp, err := server.NewResponse(in.Appid, v)
+		if err != nil {
+			continue
+		}
+		err = wxRsp.Conn.Write([]byte(in.Message))
+		if err != nil {
+			res.Count++
+		}
+	}
+	return res, nil
+
+}
